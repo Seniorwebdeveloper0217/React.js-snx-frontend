6f52f8812a205dc9430067c96998b25ba1478982
Excellent-Developer <fazylalmeeve@gmail.com>
2020-01-22 07:58:25 +0800

    remove blue underline

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,31 +19,25 @@ class Header extends Component {
                     <a className = "title" href ="/">
                         <img src = "images/title.png" />
                     </a>
-                    <a className="navbar-brand brandimage" href="/">
-                        <img src = "images/Spartans-SNX.png" />
-                    </a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="/">Link</a>
-                        </li>
-                        <li className="nav-item dropdown fixed-right">
-                            <a className="nav-link dropdown-toggle buttonstyle" href="/" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Web3 Login
-                            </a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a className="dropdown-item" href="/">Action</a>
-                            <a className="dropdown-item" href="/">Another action</a>
-                            <a className="dropdown-item" href="/">Something else here</a>
-                            </div>
-                        </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/">Link</a>
+                            </li>
+                            <li className="nav-item dropdown fixed-right">
+                                <a className="nav-link dropdown-toggle buttonstyle" href="/" id="navbardrop" role="button" data-toggle="dropdown">
+                                    Web3 Login
+                                </a>
+                                <div className="dropdown-menu">
+                                    <a className="dropdown-item" href="/">Action</a>
+                                    <a className="dropdown-item" href="/">Another action</a>
+                                    <a className="dropdown-item" href="/">Something else here</a>
+                                </div>
+                            </li>
                         </ul>
                     </div>
                 </nav>
