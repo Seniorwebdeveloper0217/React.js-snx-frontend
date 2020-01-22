1a3bc11dc803c86b6049f06a369f877d0edd88ba
Excellent-Developer <fazylalmeeve@gmail.com>
2020-01-22 13:43:18 +0800

    complete-1

--- src/pages/ProposalsPage.js
+++ src/pages/ProposalsPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom"
 import ProposalDescription from "../components/ProposalDescription";
+import "../styles/ProposalsPage.css";
 
 class ProposalsPage extends Component {
     state = {
